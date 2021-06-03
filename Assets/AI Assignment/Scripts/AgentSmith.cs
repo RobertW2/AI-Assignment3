839ec0cc658f838024f893159404cf868bcb567c
RobertW2 <bobbyweeden2@gmail.com>
2021-06-03 13:01:03 +1000

    Changes, Statemachine started

--- Assets/AI Assignment/Scripts/AgentSmith.cs
+++ Assets/AI Assignment/Scripts/AgentSmith.cs
@@ -3,40 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-
-[RequireComponent(typeof(NavMeshAgent))]
-
-public class AgentSmith : MonoBehaviour
+namespace StateMachines
 {
+    [RequireComponent(typeof(NavMeshAgent))]
 
-    private NavMeshAgent agent;
-    private WayPoint[] wayPoints;
-
-    // Will give us a random waypoint in the array as a variabl everytime i access it
-    private WayPoint RandomPoint => wayPoints[Random.Range(0, wayPoints.Length)];
-
-    // Start is called before the first frame update
-    void Start()
+    public class AgentSmith : MonoBehaviour
     {
-        agent = gameObject.GetComponent<NavMeshAgent>();
-        // FindObjectsOfType gets every instance of this component in the scene
-        wayPoints = FindObjectsOfType<WayPoint>();
 
-        // Tell the agent to move to a random position in the scene waypoints
-        agent.SetDestination(RandomPoint.Position);
-    }
+        public NavMeshAgent agent;
+        private WayPoint[] wayPoints;
 
-    // Update is called once per frame
-    void Update()
-    {
-        // Has the agent reached it's position?
-        if(!agent.pathPending && agent.remainingDistance < 0.1f)
+        // Will give us a random waypoint in the array as a variabl everytime i access it
+        private WayPoint RandomPoint => wayPoints[Random.Range(0, wayPoints.Length)];
+
+        // Start is called before the first frame update
+        void Start()
         {
+            agent = gameObject.GetComponent<NavMeshAgent>();
+            // FindObjectsOfType gets every instance of this component in the scene
+            wayPoints = FindObjectsOfType<WayPoint>();
 
             // Tell the agent to move to a random position in the scene waypoints
             agent.SetDestination(RandomPoint.Position);
         }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // Has the agent reached it's position?
+            if (!agent.pathPending && agent.remainingDistance < 0.1f)
+            {
+
+                // Tell the agent to move to a random position in the scene waypoints
+                agent.SetDestination(RandomPoint.Position);
+            }
+        }
+
+
     }
-
-
 }
---
+++ Assets/AI Assignment/Scripts/StateMachine.cs
@@ -0,0 +1,78 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+namespace StateMachines
+{
+    public enum States
+    {
+        LookingForKey,
+        LookForEnd
+    }
+
+    // The delegate that dictates what the funtions will look like.
+    public delegate void StateDelegate();
+
+    public class StateMachine : MonoBehaviour
+    {
+        private Dictionary<States, StateDelegate> states = new Dictionary<States, StateDelegate>();
+
+        [SerializeField] private States currentState = States.LookingForKey;
+        [SerializeField] private Transform controlled; // the thing that will be affected by our statemachines
+        [SerializeField] private float speed = 1f; // This isn't really in a statemachine, this is just for testing
+
+        // This is used to change states from anywhere within the code that has reference
+        // to the StateMachine
+
+        public void ChangeState(States _newState)
+        {
+            if (_newState != currentState)
+                currentState = _newState;
+        }
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            // This is the same as checking if the cariable is null, then setting it otherwise
+            // retain the value 
+            controlled ??= transform;
+
+            states.Add(States.LookingForKey, LookingForKey);
+            states.Add(States.LookForEnd, Rotate);
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // These two line are what actually runs the state machine
+            // It works by attemping to retreve the relevent funtion for the current state,
+            // then running the function if it successfully found it.
+            if (states.TryGetValue(currentState, out StateDelegate state))
+                state.Invoke();
+            else
+                Debug.LogError($"No State funtion set for state {currentState}.");
+        }
+
+        // The funtion that will run when we are in the traanslate state.
+        public void LookingForKey()
+        {
+            Key[] keys = FindObjectsOfType<Key>();
+            // Loop through keys
+            // Find closest
+            AgentSmith smith = FindObjectOfType<AgentSmith>();
+            smith.agent.SetDestination(FindObjectOfType<Key>().transform.position);
+
+
+
+            // Move towards found key
+        }
+        private void Rotate() => controlled.Rotate(Vector3.up, speed * .5f);
+        
+       
+    }
+}
+
+
